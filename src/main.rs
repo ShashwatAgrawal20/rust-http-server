d4de4ead1d122bce2ecd666bba39d63b99f12518
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2023-12-31 14:57:22 +0530

    feat(GET): hosting a directory and GET request to a file
    Note:- Not doing any input sanitization of any type, nor we are
    verifying the '--directory'
    
     - The server hosts a specific directory using the '--directory' flag
    
     - The client can get the content of the file by just making a GET
       request on 'localhost:4221/files/file_name'
    
     - we are using the 'Path' module from the std library to check if the
       absolute path exists && if that's a file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::error::Error;
-use std::io::{prelude::*, BufReader};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::{
+    error::Error,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    path::Path,
+    {env, fs, thread},
+};
 
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n";
 const NOT_FOUND: &str = "HTTP/1.1 404 Not Found\r\n";
@@ -30,6 +33,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     println!("new client!");
     let buffer = BufReader::new(&stream);
+    let args: Vec<String> = env::args().collect();
     let request: Vec<_> = buffer
         .lines()
         .map(|result| result.unwrap_or_default())
@@ -40,9 +44,25 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     match path {
         path if path.starts_with("/echo/") => handle_manual_route(stream, &path[6..])?,
         path if path.starts_with("/user-agent") => handle_manual_route(stream, user_agent)?,
+        path if path.starts_with("/files") => get_file(stream, &path[7..], args)?,
         "/" => stream.write_all(format!("{OK_RESPONSE}\r\n").as_bytes())?,
         _ => stream.write_all(format!("{NOT_FOUND}\r\n").as_bytes())?,
     };
+    Ok(())
+}
+
+fn get_file(mut stream: TcpStream, path: &str, args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    let file_path = args[2].to_string();
+    let file_name = path;
+    let absolute_path = format!("{file_path}/{file_name}");
+    if Path::new(&absolute_path).exists() && Path::new(&absolute_path).is_file() {
+        let content = fs::read_to_string(&absolute_path)?;
+        let length = content.len();
+        let response = format!("{OK_RESPONSE}Content-type: text/plain\r\nContent-Length: {length}\r\n\r\n{content}\r\n");
+        stream.write_all(response.as_bytes())?;
+    } else {
+        stream.write_all(format!("{NOT_FOUND}\r\n").as_bytes())?;
+    }
     Ok(())
 }
 
